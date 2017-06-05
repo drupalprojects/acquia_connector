8a43f430b1ec4b8b58b68bfec3c8cd43907d1a05
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2017-06-05 09:24:24 -0400

    Removed static usages of Subscription

--- src/Form/CredentialForm.php
+++ src/Form/CredentialForm.php
@@ -142,14 +142,15 @@ class CredentialForm extends ConfigFormBase {
 
     // Check subscription and send a heartbeat to Acquia Network via XML-RPC.
     // Our status gets updated locally via the return data.
-    $subscription = Subscription::update();
+    $subscription = new Subscription();
+    $subscription_data = $subscription->update();
 
     // Redirect to the path without the suffix.
     $form_state->setRedirect('acquia_connector.settings');
 
     drupal_flush_all_caches();
 
-    if ($subscription['active']) {
+    if ($subscription_data['active']) {
       drupal_set_message($this->t('<h3>Connection successful!</h3>You are now connected to Acquia Cloud. Please enter a name for your site to begin sending profile data.'));
     }
   }
