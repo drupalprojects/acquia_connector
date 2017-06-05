cf163ba6ad24cfc3d29b28c022551dd10f5b9ff1
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2017-06-05 10:52:26 -0400

    Small refactoring

--- src/AutoConnector.php
+++ src/AutoConnector.php
@@ -3,23 +3,36 @@
 namespace Drupal\acquia_connector;
 
 use Drupal\acquia_connector\Helper\Storage;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 
 /**
- * Class Subscription.
+ * Class AutoConnector.
  *
  * @package Drupal\acquia_connector.
  */
 class AutoConnector {
 
-  public function __construct(Subscription $subscription, Storage $storage, $user, array $global_config) {
+  /**
+   * AutoConnector constructor.
+   *
+   * @param Subscription $subscription
+   * @param Storage $storage
+   * @param AccountInterface $user
+   * @param array $global_config
+   */
+  public function __construct(Subscription $subscription, Storage $storage, AccountInterface $user, array $global_config) {
     $this->subscription = $subscription;
     $this->storage = $storage;
     $this->global_config = $global_config;
     $this->user = $user;
   }
 
-
+  /**
+   * Ensures a connection to Acquia Subscription.
+   *
+   * @return bool|mixed
+   */
   public function ensure() {
 
     if ($this->subscription->hasCredentials()) {
@@ -40,14 +53,19 @@ class AutoConnector {
     $activated = $this->subscription->update();
 
     if ($activated && $this->user->hasPermission('administer site configuration')) {
-      if (function_exists('t') && function_exists('drupal_set_message')) {
-        $url = Url::fromRoute('acquia_connector.setup');
-        $text = t('Your site has been automatically connected to Acquia. <a href="!url">Change subscription</a>', array('!url' => $url));
-        drupal_set_message($text, 'status', FALSE);
-      }
+      $this->showMessage();
     }
 
     return $activated;
 
   }
+
+  protected function showMessage() {
+    if (function_exists('t') && function_exists('drupal_set_message')) {
+      $url = Url::fromRoute('acquia_connector.setup');
+      $text = t('Your site has been automatically connected to Acquia. <a href="!url">Change subscription</a>', ['!url' => $url]);
+      drupal_set_message($text, 'status', FALSE);
+    }
+  }
+
 }
